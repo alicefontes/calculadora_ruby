750aabd3610e68c2c9f1ab365b5de70a195c2b05
Alice Fontes <alice.fontes@Mac-2.local>
2016-08-11 12:21:35 -0300

    calculadora identifica numeros e sinais

--- lib/calculadora_ruby.rb
+++ lib/calculadora_ruby.rb
@@ -1,5 +1,39 @@
 require "calculadora_ruby/version"
 
 module CalculadoraRuby
-  # Your code goes here...
+  class Calc
+    def initialize calc_string
+      @calc_array = calc_string.split('')
+      #avisaquenasceu
+      operacoes_calc
+    end
+
+    def avisaquenasceu
+      puts @calc_array
+    end
+
+    def operacoes_calc
+      index = @calc_array.index { |x| x == "+" }
+      n1 = array[index - 1]
+      op = array[index]
+      n2 = array[index + 1]
+    end
+
+    if op === "+"
+        puts resultadosoma
+    elsif op === "-"
+        puts resultadodiferenca
+    else
+        puts 'erro'
+    end
+
+    def + n2
+      resultadosoma = n1
+      resultadosoma += n2
+    end
+    def - n2
+      resultadodiferenca = n1
+      resultadodiferenca -= n2
+    end
+  end
 end
